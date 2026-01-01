048d1410a9de4a8fb8816cea88eb70463a72aae8
kuber4s <kuber4s@yahoo.com>
2026-01-01 04:12:53 +0300

    Initial commit

---
+++ src/main/java/com/discord/balancer/MixBotApplication.java
@@ -0,0 +1,13 @@
+package com.discord.balancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixBotApplication.class, args);
+	}
+
+}
